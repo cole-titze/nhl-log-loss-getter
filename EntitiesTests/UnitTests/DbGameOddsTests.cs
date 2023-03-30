5a30292af426f988eec21492d51cca1e799a6f26
Cole <cole.titze@outlook.com>
2023-03-29 20:41:13 -0500

    Added remaining placeholder tests

---
+++ EntitiesTests/UnitTests/DbGameOddsTests.cs
@@ -0,0 +1,32 @@
+﻿using Entities.DbModels;
+using FluentAssertions;
+
+namespace EntitiesTests.UnitTests
+{
+    [TestClass]
+    public class DbGameOddsTests
+    {
+        [TestMethod]
+        public void FutureTestsHere()
+        {
+            var game = new DbGameOdds()
+            {
+                gameId = 1,
+                bovadaOpeningVegasHomeOdds = .77,
+                bovadaOpeningVegasAwayOdds = .33,
+                myBookieOpeningVegasAwayOdds = .23,
+                myBookieOpeningVegasHomeOdds = .88,
+                pinnacleOpeningVegasAwayOdds = .21,
+                pinnacleOpeningVegasHomeOdds = .91,
+                betOnlineOpeningVegasAwayOdds = .88,
+                betOnlineOpeningVegasHomeOdds = .32,
+                bet365OpeningVegasAwayOdds = .23,
+                bet365OpeningVegasHomeOdds = .98,
+                modelHomeOdds = .78,
+                modelAwayOdds = .22,
+            };
+
+            game.modelHomeOdds.Should().Be(.78);
+        }
+    }
+}
